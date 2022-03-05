938afc203ee796f63fe9326141430202e73f268f
amorimlt <julianeamorim.lt@gmail.com>
2022-03-04 22:38:36 -0300

    finaliza exercicío

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine(" ");
+Console.WriteLine("Solidão de Volta");
+Console.ResetColor();
+Console.WriteLine(" ");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine("Cê já tem a solidão de volta");
+Console.WriteLine("Não precisa mais se explicar");
+Console.WriteLine("Onde vai a ninguém mais importa");
+Console.ResetColor();
+Console.WriteLine(" ");
+
+Console.WriteLine("Onde vai agora?");
+Console.WriteLine(" ");
+
+Console.WriteLine(@"Ouve-se que um belo horizonte
+Daqui de longe, não posso fazer nada
+Mas certamente, certamente
+Respirar
+Só tem um jeito");
+Console.WriteLine(" ");
+
+Console.ForegroundColor = ConsoleColor.DarkRed;
+Console.WriteLine(@"Eu já tenho a solidão de volta
+Não preciso mais me explicar
+Onde vou a ninguém mais importa
+Onde vou agora é");
+Console.WriteLine(" ");
